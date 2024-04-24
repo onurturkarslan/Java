b80544b63cbc9970cd9962baaed33fe2f277d9a8
Onur <onurturkarslanonur@gmail.com>
2024-04-24 15:46:20 +0300

    first commit

--- src/Week1/Kdv.java
+++ src/Week1/Kdv.java
@@ -6,17 +6,28 @@ import java.util.Scanner;
 public class Kdv {
     public static void main(String[] args) {
 
-        //Değişkeni oluşturduk.
-        double para;
-        double kdv = 0.18;
+        //Değişkenleri oluşturduk.
+        double para, kdvliFiyat, kdvTutari;
 
         //Scanner sınıfını tanımladık.
         Scanner girdi = new Scanner(System.in);
 
+        System.out.print("Lütfen bir tutar giriniz: ");
         para = girdi.nextInt();
+        if (para > 0) {
+            double kdv;
+            if (para <= 1000) {
+                kdv = 0.18;
+            } else {
+                kdv = 0.08;
+            }
 
-        double sonuc = (para * kdv) + para;
-        System.out.println(sonuc);
+            kdvTutari = para * kdv;
+            kdvliFiyat = (para * kdv) + para;
+            System.out.println("Kdv'li Fiyat :" + kdvliFiyat);
+            System.out.println("Kdv Tutarı: " + kdvTutari);
+        } else {
+            System.out.println("Lütfen sıfırdan büyük bir değer giriniz!");
+        }
     }
-
 }
