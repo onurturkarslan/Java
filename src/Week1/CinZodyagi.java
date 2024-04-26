b06d8cd0918292a1c8d4a59c7659c3bb8fd83956
Onur <onurturkarslanonur@gmail.com>
2024-04-26 15:42:37 +0300

    first commit

---
+++ src/Week1/CinZodyagi.java
@@ -0,0 +1,40 @@
+package Week1;
+import java.util.Scanner;
+public class CinZodyagi {
+    public static void main(String[] args) {
+        Scanner girdi = new Scanner(System.in);
+
+        System.out.print("Doğum yılınızı giriniz: ");
+        int dogumYili = girdi.nextInt();
+
+        int cinZodyagi = dogumYili % 12;
+
+        if (cinZodyagi == 0){
+            System.out.println("Çin Zodyağı Burcunuz: Maymun");
+        } else if (cinZodyagi == 1) {
+            System.out.println("Çin Zodyağı Burcunuz: Horoz");
+        }else if (cinZodyagi == 2) {
+            System.out.println("Çin Zodyağı Burcunuz: Köpek");
+        }else if (cinZodyagi == 3) {
+            System.out.println("Çin Zodyağı Burcunuz: Domuz");
+        }else if (cinZodyagi == 4) {
+            System.out.println("Çin Zodyağı Burcunuz: Fare");
+        }else if (cinZodyagi == 5) {
+            System.out.println("Çin Zodyağı Burcunuz: Öküz");
+        }else if (cinZodyagi == 6) {
+            System.out.println("Çin Zodyağı Burcunuz: Kaplan");
+        }else if (cinZodyagi == 7) {
+            System.out.println("Çin Zodyağı Burcunuz: Tavşan");
+        }else if (cinZodyagi == 8) {
+            System.out.println("Çin Zodyağı Burcunuz: Ejderha");
+        }else if (cinZodyagi == 9) {
+            System.out.println("Çin Zodyağı Burcunuz: Yılan");
+        }else if (cinZodyagi == 10) {
+            System.out.println("Çin Zodyağı Burcunuz: At");
+        }else {
+            System.out.println("Çin Zodyağı Burcunuz: Koyun");
+        }
+
+
+    }
+}
