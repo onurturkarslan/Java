9577a9275145525f0960973ac9df2a51fba0dd76
Onur <onurturkarslanonur@gmail.com>
2024-05-04 19:00:16 +0300

    first commit

---
+++ src/Week2/Employee.java
@@ -0,0 +1,64 @@
+package Week2;
+
+import java.time.Year;
+
+public class Employee {
+    private String name;
+    private double salary;
+    private int workHours;
+    private int hireYear;
+
+    public Employee(String name, double salary, int workHours, int hireYear) {
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+    }
+
+    public double tax() {
+        if (salary < 1000) {
+            return 0;
+        } else {
+            return salary * 0.03;
+        }
+    }
+
+    public double bonus() {
+        if (workHours > 40) {
+            return (workHours - 40) * 30;
+        } else {
+            return 0;
+        }
+    }
+
+    public double raiseSalary() {
+        int yearsWorked = Year.now().getValue() - hireYear;
+        if (yearsWorked < 10) {
+            return salary * 0.05;
+        } else if (yearsWorked < 20) {
+            return salary * 0.10;
+        } else {
+            return salary * 0.15;
+        }
+    }
+
+    @Override
+    public String toString() {
+        double totalSalary = salary - tax() + bonus() + raiseSalary();
+        return "Adı: " + name +
+                "\nMaaşı: " + salary +
+                "\nÇalışma Saati: " + workHours +
+                "\nBaşlangıç Yılı: " + hireYear +
+                "\nVergi: " + tax() +
+                "\nBonus: " + bonus() +
+                "\nMaaş Artışı: " + raiseSalary() +
+                "\nVergi ve Bonuslar ile birlikte maaş: " + (totalSalary - salary) +
+                "\nToplam Maaş: " + totalSalary;
+    }
+
+    public static void main(String[] args) {
+        Employee emp = new Employee("kemal", 2000.0, 45, 1985);
+        System.out.println(emp.toString());
+    }
+}
+
